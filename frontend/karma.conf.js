de821d68e11e6b258d04dd5242c7443be4fffc86
Seongrok Shin <ssr7324@gmail.com>
2025-03-19 18:40:33 +1300

    fix: Test CI pipeline

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,6 +1,17 @@
 module.exports = function (config) {
   config.set({
     browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: "Chrome",
+        flags: [
+          "--headless",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
     captureTimeout: 60000,
     browserDisconnectTimeout: 60000,
     browserDisconnectTolerance: 3,
