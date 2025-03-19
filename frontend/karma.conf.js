6cd6eddd02f873ca730753515978433cc3435755
Seongrok Shin <ssr7324@gmail.com>
2025-03-19 18:36:49 +1300

    fix: add more conf and chromium for testing

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,5 +1,9 @@
 module.exports = function (config) {
   config.set({
     browsers: ["ChromeHeadless"],
+    captureTimeout: 60000,
+    browserDisconnectTimeout: 60000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
   });
 };
